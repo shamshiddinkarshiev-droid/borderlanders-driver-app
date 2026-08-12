09a5ad8cd0cdcaf6756ecb72521ef255cfa27c0b
Shamshiddin <shamshiddinkarshiev@gmail.com>
2026-08-12 17:54:12 +0500

    fix uploadthing helpers

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,7 +1,6 @@
-import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react";
-import { useUploadThing } from "@uploadthing/react";
+import { generateUploadButton, generateUploadDropzone, generateReactHelpers } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
-export { useUploadThing };
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
