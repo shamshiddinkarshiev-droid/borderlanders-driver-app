838233650e87815caff2f207f138a2d37bc9e07b
Shamshiddin <shamshiddinkarshiev@gmail.com>
2026-08-15 15:00:54 +0500

    downgrade uploadthing v6 config

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -6,17 +6,13 @@ export const ourFileRouter = {
   documentUploader: f({
     image: { maxFileSize: "4MB", maxFileCount: 1 },
     pdf: { maxFileSize: "4MB", maxFileCount: 1 },
-  }, { awaitServerData: false })
-  .middleware(() => ({}))
-  .onUploadComplete(({ file }) => {
-    return { url: file.ufsUrl || file.url };
+  }).onUploadComplete(async ({ file }) => {
+    return { url: file.url };
   }),
   photoUploader: f({
     image: { maxFileSize: "4MB", maxFileCount: 1 },
-  }, { awaitServerData: false })
-  .middleware(() => ({}))
-  .onUploadComplete(({ file }) => {
-    return { url: file.ufsUrl || file.url };
+  }).onUploadComplete(async ({ file }) => {
+    return { url: file.url };
   }),
 } satisfies FileRouter;
 
