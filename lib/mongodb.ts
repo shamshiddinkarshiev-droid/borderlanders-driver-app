ae854c4f061fff6c41732a89ec40f45335d55649
Shamshiddin <shamshiddinkarshiev@gmail.com>
2026-08-16 14:05:06 +0500

    fix mongodb export

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error(
-    "Please define MONGODB_URI in your .env.local file"
-  );
+  throw new Error("Please define MONGODB_URI in your .env.local file");
 }
 
 interface MongooseCache {
@@ -14,28 +12,19 @@ interface MongooseCache {
 }
 
 declare global {
-  // eslint-disable-next-line no-var
   var mongooseCache: MongooseCache | undefined;
 }
 
-const cached: MongooseCache =
-  global.mongooseCache || {
-    conn: null,
-    promise: null,
-  };
-
+const cached: MongooseCache = global.mongooseCache || { conn: null, promise: null };
 global.mongooseCache = cached;
 
-export async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI);
+    cached.promise = mongoose.connect(MONGODB_URI!);
   }
-
   cached.conn = await cached.promise;
-
   return cached.conn;
 }
+
+export default dbConnect;
